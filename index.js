b4a04cd8f6739dab2c87c4e6a8cedcb87c0262f0
Nathank4242 <nkweon@gmail.com>
2024-10-09 13:56:11 -0400

    dom

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const DOMselectors = {
-  header: document.querySelector("h1"),
+  header: document.querySelectorAll("h1"),
   item: document.querySelectorAll("li"),
   card: document.querySelector(".card"),
   button: document.querySelector(".btn"),
   form: document.querySelector(".form"),
-};
-
-let student = {
-  name: "student 1",
-  age: 17,
-  gf: true,
+  conatiner: document.querySelector(".container"),
 };
 
 console.log(DOMselectors.card);
@@ -21,6 +16,41 @@ DOMselectors.button.addEventListener("click", function (event) {
 
 DOMselectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-
-  console.log(document.querySelector("input").value);
+  let = input = document.querySelector("input");
+  console.group(input.value);
+  input.value = ";";
+  console.log(input.value);
 });
+
+// select all buttons as node listt
+const buttons = document.querySelectorAll("button");
+// make array
+const newButtons = Array.from(buttons);
+// iterate through array and change each buttons color
+newButtons.forEach((button) => (button.style.backgroundColor = "blue"));
+
+newButtons.forEach((button) =>
+  button.addEventListener("click", function (event) {
+    console.log(event.target.textContent);
+  })
+);
+
+let movie = {
+  title: "Star Wars",
+  release: 1977,
+};
+
+DOMselectors.container.insertAdjacentHTML(
+  "beforeend",
+  `<div class="card"><h2 class= "card=header">${movie.title}</h2></div>`
+);
+
+// create the html for inmputs , card and container aka nwhere the card s go
+
+// select/query the htmol form and get value from inputs
+
+//turn vlaues into pbject Movie, Game, User whatevs
+
+//insert card with object onto html
+
+//add event listener for remove button in JS
